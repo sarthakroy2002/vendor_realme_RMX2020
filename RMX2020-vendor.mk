3a65fe0d169e68f49d9108b23a1bd0bb19dad785
imranpopz <imranpopz32@gmail.com>
2021-05-06 17:14:34 +0530

    RMX2020 : Use libdpframework.so and libvpud_vcodec.so from stock A02
    Co-authored-by: Maanush Putcha <p.maanush@gmail.com>
    Co-authored-by: SamarV-121 <samarvispute121@gmail.com>

--- RMX2020-vendor.mk
+++ RMX2020-vendor.mk
@@ -18,6 +18,11 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/realme/RMX2020
 
 PRODUCT_COPY_FILES += \
+    vendor/realme/RMX2020/proprietary/product/vendor_overlay/29/lib/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libdpframework.so \
+    vendor/realme/RMX2020/proprietary/product/vendor_overlay/29/lib/libvpud_vcodec.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libvpud_vcodec.so \
+    vendor/realme/RMX2020/proprietary/product/vendor_overlay/29/lib64/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libdpframework.so
+
+PRODUCT_COPY_FILES += \
     vendor/realme/RMX2020/proprietary/bin/kpoc_charger:$(TARGET_COPY_OUT_SYSTEM)/bin/kpoc_charger \
     vendor/realme/RMX2020/proprietary/product/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml \
     vendor/realme/RMX2020/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
